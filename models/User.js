0e5d73942e0ad64ebf43714b2508face15a4395b
Sam Jones <samejones2018@gmail.com>
2023-05-23 18:11:09 -0700

    added LOTS of boiler plate code

---
+++ models/User.js
@@ -0,0 +1,53 @@
+const { Model, DataTypes } = require('sequelize'); 
+const sequelize = require('../config/connection'); 
+const bcrypt = require('bcrypt'); // Import the bcrypt library for password hashing
+
+class User extends Model {
+  async checkPassword(loginPw) {
+    return await bcrypt.compare(loginPw, this.password); // Compare the login password with the hashed password
+  }
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true, // Ensures that each username is unique
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true, // Ensures that each email is unique
+        validate: {
+            isEmail: true, // Uses a regular expression to validate whether the email is in the correct format
+        },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8], 
+      },
+    },
+    profile_picture: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+  },
+  {
+    sequelize, // Connects the model to the Sequelize instance
+    timestamps: false, 
+    freezeTableName: true, 
+    underscored: true, 
+    modelName: 'user', 
+  }
+);
+
+module.exports = User; 
---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+// const routes = require('./routes');
+const sequelize = require('./config/connection');
+const cookieParser = require('cookie-parser');
+const hbs = exphbs.create({});
+
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+
+app.use(
+    session({
+      name: 'my_app.sid', // Set the name of the session cookie
+      secret: process.env.SESSION_SECRET, // Set the session secret from environment variable
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        maxAge: 24 * 60 * 60 * 1000, // Set the session cookie expiration time to 24 hours
+      },
+    })
+  );
+
+  app.use((req, res, next) => {
+    // Clear the session cookie if user_sid is present but the user is not logged in
+    if (req.cookies.user_sid && !req.session.logged_in) {
+      res.clearCookie('my_app.sid');
+    }
+    next();
+  });
+
+
+    // app.use(routes);
+
+
+sequelize.sync({ force: false }).then(() => { // Sync the database models with the database
+    app.listen(PORT, () => {
+      console.log(`App listening on port ${PORT}!`);
+    });
+  });
