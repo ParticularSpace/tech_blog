7d756487899752d2551c45cb6967758183dbb2b1
Sam Jones <samejones2018@gmail.com>
2023-06-06 17:40:18 -0700

    heroku

--- migrations/20230607001624-removePhone.js
+++ migrations/20230607001624-removePhone.js
@@ -3,13 +3,6 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.removeColumn('User', 'phone_number');
+    await queryInterface.removeColumn('Users', 'phone_number');
   },
-
-  async down (queryInterface, Sequelize) {
-    await queryInterface.addColumn('User', 'phone_number', {
-      type: Sequelize.STRING,
-      allowNull: true
-    });
-  }
 };
